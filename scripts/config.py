f4bfa0cc55dc38aaa7b7137367bcc3846f1a4152
jonad <jonadjons@gmail.com>
2020-02-17 19:21:59 -0800

    Added the datasets file

--- scripts/config.py
+++ scripts/config.py
@@ -1,5 +1,10 @@
 import torch
 from typing import TypeVar
+
+TRAINING_FOLDER = '/path/to/training'
+TEST_FOLDER_NORMAL = '/path/to/test/folder'
+TEST_FOLDER_CANCER = '/path/to/test/cancer'
+
 image_size = 64
 n_channels = 3
 n_latent_vector = 100
@@ -12,3 +17,4 @@ learning_rate=0.0002
 batch_size = 64
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 T_co = TypeVar('T_co', covariant=True)
+
---
+++ scripts/datasets.py
@@ -0,0 +1,30 @@
+from torchvision import datasets
+from torchvision import transforms
+
+
+from .config import *
+
+
+training_images = datasets.ImageFolder(root=TRAINING_FOLDER,
+                           transform=transforms.Compose([
+                               transforms.Resize(image_size),
+                               transforms.CenterCrop(image_size),
+                               transforms.ToTensor(),
+                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                           ]))
+
+test_normal_images = datasets.ImageFolder(root=TEST_FOLDER_NORMAL,
+                           transform=transforms.Compose([
+                               transforms.Resize(image_size),
+                               transforms.CenterCrop(image_size),
+                               transforms.ToTensor(),
+                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                           ]))
+
+test_cancer_images = datasets.ImageFolder(root=TEST_FOLDER_CANCER,
+                           transform=transforms.Compose([
+                               transforms.Resize(image_size),
+                               transforms.CenterCrop(image_size),
+                               transforms.ToTensor(),
+                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                           ]))
